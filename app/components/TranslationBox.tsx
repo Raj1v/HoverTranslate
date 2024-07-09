342aa433ffb436162a94eef990e592994f2e5584
Frederik Rajiv Manichand <rajivmanichand@gmail.com>
2024-07-09 16:58:12 +0200

    Proper fade out of translation box

--- app/components/TranslationBox.tsx
+++ app/components/TranslationBox.tsx
@@ -2,20 +2,34 @@ import { useEffect, useState, useRef } from "react";
 import "@/app/styles/tooltips.css";
 
 const TranslationBox = (props: {
-  translation: string;
+  translation: string | undefined;
   position: { x: number; y: number };
 }) => {
   const ref = useRef<HTMLParagraphElement>(null);
   const { translation, position } = props;
+  const [displayedTranslation, setDisplayedTranslation] = useState(translation);
 
   useEffect(() => {
-    // Callculate the proper position for the tooltipbox based on the width of the tooltipbox
+    // Calculate the proper position for the tooltipbox based on the width of the tooltipbox
     if (ref.current) {
       const x = position.x - ref.current.offsetWidth / 2;
       const y = position.y + window.scrollY - 30;
       ref.current.style.transform = `translate3d(${x}px, ${y}px, 0)`;
     }
   });
+
+  useEffect(() => {
+    if (translation) {
+      setDisplayedTranslation(translation);
+    } else {
+      // Delay clearing the translation to allow for fade-out effect
+      const timeoutId = setTimeout(
+        () => setDisplayedTranslation(translation),
+        300
+      ); // Adjust 300ms to match your fade-out duration
+      return () => clearTimeout(timeoutId);
+    }
+  }, [translation]);
 
   return (
     <div role="dialog" aria-labelledby="share" aria-haspopup="dialog">
@@ -27,7 +41,7 @@ const TranslationBox = (props: {
           opacity: translation && position ? 1 : 0,
         }}
       >
-        {translation}
+        {displayedTranslation}
       </p>
     </div>
   );
